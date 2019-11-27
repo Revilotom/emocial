ffc20d93e11728c1d3c95777d3b451b1d82f57b5
Tom Oliver <tom.oliver@bizreach.co.jp>
2019-11-27 11:23:46 +0900

    Finally fixed not being able to display emojis on the production server

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,8 +1,10 @@
-PLAYDIR="play-java-jpa-example"
+PLAYDIR="../play-java-jpa-example"
 
 echo Building play application...
 
 (cd $PLAYDIR && sbt dist)
+
+cp $PLAYDIR/target/universal/*.zip ./deployment/
 
 echo Building image..
 
@@ -11,9 +13,10 @@ docker save -o theSave 19-tom-oliver:latest
 
 echo Copying docker image...
 
-scp -i ~/Downloads/FinalProject.pem -rC ~/Documents/training/finalProject/19-tom-oliver/theSave ec2-user@13.112.1.50:~/
+scp -i ~/Downloads/FinalProject.pem -rC ~/Documents/training/finalProject/19-tom-oliver/deployment/theSave ec2-user@13.112.1.50:~/
 
 rm ./theSave
+rm ./*.zip
 
 echo Starting server...
 
