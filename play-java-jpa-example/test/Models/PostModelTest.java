bc29795209ebb510e92b8ab4e2f52c7d1ba7c022
Tom Oliver <tom.oliver@bizreach.co.jp>
2019-11-27 11:42:28 +0900

    Fixed emoji detection for dark skin

--- play-java-jpa-example/test/Models/PostModelTest.java
+++ play-java-jpa-example/test/Models/PostModelTest.java
@@ -1,5 +1,6 @@
 package Models;
 
+import com.vdurmont.emoji.EmojiParser;
 import models.Post;
 import org.hamcrest.MatcherAssert;
 import org.junit.Test;
@@ -14,6 +15,17 @@ public class PostModelTest {
                 "\uDE01\uD83D\uDE0D\uD83D\uDE17\uD83D\uDE17\uD83D\uDE19");
         MatcherAssert.assertThat(p.validate(), is(nullValue()));
     }
+
+    @Test
+    public void testDarkSkin() {
+        String content = "\uD83D\uDC69\uD83C\uDFFF\u200D\uD83C\uDF3E";
+        System.out.println("[" + EmojiParser.removeAllEmojis(content) + "]");
+        Post p = new Post(content);
+        MatcherAssert.assertThat(p.validate(), is(nullValue()));
+
+
+    }
+
     @Test
     public void testValidationNonEmojis() {
         Post p = new Post("\uD83D\uDC79asd\uD83D\uDE24\uD83D\uDE36\uD83D\uDE11\uD83E\uDD14\uD83D" +
