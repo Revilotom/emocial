45c36c2f5e4ae49998e187aa18b6c70afe7f0970
Tom Oliver <tom.oliver@bizreach.co.jp>
2019-11-29 11:43:34 +0900

    Changed from micro to small instance

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -13,7 +13,7 @@ docker save -o theSave 19-tom-oliver:latest
 
 echo Copying docker image...
 
-scp -i ~/Downloads/FinalProject.pem -rC ~/Documents/training/finalProject/19-tom-oliver/deployment/theSave ec2-user@13.112.1.50:~/
+scp -i ~/Downloads/FinalProject.pem -rC ~/Documents/training/finalProject/19-tom-oliver/deployment/theSave ec2-user@18.179.113.178:~/
 
 rm ./theSave
 rm ./*.docker_temp*
--- deployment/remoteStart.sh
+++ deployment/remoteStart.sh
@@ -1,2 +1,2 @@
 ./remoteKill.sh
-ssh -i ~/Downloads/FinalProject.pem ec2-user@13.112.1.50 'bash -s' < ./start.sh
+ssh -i ~/Downloads/FinalProject.pem ec2-user@18.179.113.178 'bash -s' < ./start.sh
