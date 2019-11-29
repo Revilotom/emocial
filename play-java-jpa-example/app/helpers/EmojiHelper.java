3cb8fae3a5e1caecc3bd088fb41c92de9b79d535
Tom Oliver <tom.oliver@bizreach.co.jp>
2019-11-29 18:27:44 +0900

    added number to emoji

--- play-java-jpa-example/app/helpers/EmojiHelper.java
+++ play-java-jpa-example/app/helpers/EmojiHelper.java
@@ -1,8 +1,36 @@
 package helpers;
 
+import java.util.HashMap;
+import java.util.stream.Collectors;
+
 public class EmojiHelper {
-    public static String intToEmojis(){
-        return null;
+
+    static HashMap<String, String> numToEmoji = new HashMap<String, String>() {{
+
+        put("1", "1️⃣");
+        put("2", "2️⃣");
+        put("3", "3️⃣");
+        put("4", "4️⃣");
+        put("5", "5️⃣");
+        put("6", "6️⃣");
+        put("7", "7️⃣");
+        put("8", "8️⃣");
+        put("9", "9️⃣");
+        put("0", "0️⃣");
+        put("-", "➖");
+    }};
+
+
+    public static String intToEmojis(int value) {
+
+        final String[] result = {""};
+
+        Integer.toString(value).chars()
+                .mapToObj(e -> (char) e)
+                .map(c -> numToEmoji.get(Character.toString(c)))
+                .collect(Collectors.toList()).forEach(s -> result[0] += s);
+
+        return result[0];
 
     }
 }
--- play-java-jpa-example/test/TestEmojiHelpers.java
+++ play-java-jpa-example/test/TestEmojiHelpers.java
@@ -1,8 +1,21 @@
+import helpers.EmojiHelper;
+import org.hamcrest.MatcherAssert;
 import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
 
 public class TestEmojiHelpers {
     @Test
-    public void testInt2Emoji() {
+    public void testInt2EmojiSingle() {
+        MatcherAssert.assertThat(EmojiHelper.intToEmojis(4), is("4️⃣"));
+    }
 
+    @Test
+    public void testInt2EmojiTriple() {
+        MatcherAssert.assertThat(EmojiHelper.intToEmojis(101), is("1️⃣0️⃣1️⃣"));
+    }
+    @Test
+    public void testInt2EmojiNegative() {
+        MatcherAssert.assertThat(EmojiHelper.intToEmojis(-101), is("➖1️⃣0️⃣1️⃣"));
     }
 }
