fc981651f3628314dfa7969f292c4924fcb0c8c9
Tom Oliver <tom.oliver@bizreach.co.jp>
2019-12-05 19:46:01 +0900

    Added Japanese translation

--- emocial/test/AcceptanceTest.java
+++ emocial/test/AcceptanceTest.java
@@ -10,7 +10,7 @@ public class AcceptanceTest {
     @Test
     public void test() {
         running(testServer(3333, fakeApplication(inMemoryDatabase())), HTMLUNIT, browser -> {
-            browser.goTo("http://localhost:9000");
+            browser.goTo("http://localhost:3333");
             assertThat(browser.pageSource(), containsString("Login"));
         });
     }
